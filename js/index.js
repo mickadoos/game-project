41774e1e0ebe36288cb8c6bdbaa3089cb8b38343
Yabel <yabelrr@gmail.com>
2022-09-24 09:03:01 +0200

    Setting up the project

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// class Game {
+//     constructor() {
+//         this.canvas = document.getElementById('canvas');
+//         this.ctx = canvas.getContext('2d');
+
+//     }
+// }
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+function startGame() {}
+
+window.onload = () => {
+
+}
